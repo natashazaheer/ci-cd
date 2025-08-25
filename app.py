bcad9d691cf58f2d9d2c8cf38c8b1af4f4355978
Natasha Zaheer <79361539+natashazaheer@users.noreply.github.com>
2025-08-25 15:27:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def home():
     </head>
     <body>
         <div class="container">
-            <h1 class="header">🐍 HELLO DEVOPS TEAM:)</h1>
+            <h1 class="header">🐍 HELLO DEVOPS TEAM!h1>
             <p>This is a Python Flask web application running on Ubuntu server!</p>
             <p><strong>Server Details:</strong></p>
             <ul>
